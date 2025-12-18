6d92934a05241c964a956316149ab30d01487cf5
jonathanp568 <jonathanpan0510@icloud.com>
2025-12-18 10:08:53 -0500

    Update elevator.py

--- elevator.py
+++ elevator.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from Player import Player
 import random 
 class elevator:
@@ -14,3 +15,27 @@ class elevator:
             print("no food found")
 
 elevator.floors
+=======
+# import random 
+# From Player import Playerstats
+# class elevator:
+#     def __init__(self, enemy):
+#         self.enemy = enemy
+#     def floorssupplies(self):
+#         foodchance = random.randint(1, 2)
+#         healschance = random.randint(1, 8)
+#         if healschance == 1:
+#             player.bandages += 1
+#             print("bandage found.")
+#         elif healschance == 2:
+#             player.medkits += 1
+#             print("medkit found")
+#         else:
+#             print("no supplies found")
+#         if foodchance == 1:
+#             foodamount = random.randint(5, 10)
+#             player.supplies += foodamount
+#             print(f"{foodamount} food found.")
+#         else: 
+#             print("no food found.")
+>>>>>>> elevator
