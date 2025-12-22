32d2543ebab11a24252abab059651caa849e7cc5
jonathanp568 <jonathanpan0510@icloud.com>
2025-12-22 10:16:37 -0500

    Update Player.py

--- Player.py
+++ Player.py
@@ -1,45 +1,69 @@
 import random
 import time
 import tkinter as tk
-import keyboard
 
-class Player:
-    def __init__(self, hunger, energy, bandages, medkits, foodsupply, hp):
+class player:
+    def __init__(self, hunger, energy, bandages, medkits, beans, candy, hp):
         self.hunger = hunger
         self.energy = energy
         self.hp = hp
         self.bandages = bandages
         self.medkits = medkits
-        self.foodsupply = foodsupply
+        self.beans = beans
+        self.candy = candy
+    def eatb(self):
+        if self.beans < 1:
+            print("no beans")
+        self.beans -= 1
+        self.hunger += 40
+        self.energy += 5
+    def eatc(self):
+        if self.candy < 1:
+            print("no candy")
+        elif self.candy > 0:
+            self.candy -= 1
+            self.hunger += 20
+            self.energy += 70
+    def useb(self):
+        if self.bandages < 1:
+            print("no bandages")
+        elif self.bandages > 0:
+            self.bandages -= 1
+            self.hp += 15
+    def usem(self):
+        if self.medkits < 1:
+            print("no medkits")
+        elif self.medkits > 0:
+            self.medkits -= 1
+            self.hp += 75
     def ignore(self):
-        self.hunger -= 50
+        self.hunger -= 20
     def run(self):
-        self.hunger -= 15
-        self.energy -= 50
-    def walk(self):
-        self.hunger -= 5
-        self.energy -= 10
+        self.hunger -= 10
+        self.energy -= 30
+    def explore(self):
+
+        elevator.floorssupplies()
     def DOT(self):
         self.hp -= 10
 
 class elevator:
-    def __init__(self, food, supplies, enemy):
-        self.food = food
-        self.suppies = supplies
-        self.enemy = enemy
-    def floors(self):
-        foodchance = random.randint(1, 2)
+    def floorssupplies():
+        foodchance = random.randint(1, 5)
+        healschance = random.randint(1, 16)
+        if healschance <= 4:
+            player.bandages += 1
+            print("bandage found.")
+        elif healschance == 5:
+            player.medkits += 1
+            print("medkit found")
+        else:
+            print("no supplies found")
         if foodchance == 1:
-            self.food += random.randint(5, 10)
-            print(f"{self.food} food found.")
+            player.beans += 1
+            print("you found canned beans.")
+        elif foodchance == 2:
+            player.candy += 1
+            print("you found a candy bar.")
         else: 
-            print("no food found")
-
-root = tk.Tk()
-
-root.title("Scary elevator")
-root.geometry("2000x2000") 
-
-label = tk.Label(root, text="SCARY ELEVATOR", font="Creepster")
-label.pack(pady=20) 
-root.mainloop()
+            print("no food found.")
