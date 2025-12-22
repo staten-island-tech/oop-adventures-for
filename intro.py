87c28612750cb3ce340c21ca587fd09fec07e957
boscot553 <yizhengtian2011@gmail.com>
2025-12-22 09:46:42 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,12 +1,20 @@
 import tkinter as tk
 
+
+def startcommand():
+    print("start")
+    #Command here
+
 root = tk.Tk()
 
 root.title("Scary elevator")
 root.geometry("2000x2000") 
 
-label = tk.Label(root, text="SCARY ELEVATOR", font=("Creepster", 50), fg="red", bg="black")
-label.pack(pady=20) 
+intro = tk.Label(root, text="SCARY ELEVATOR", font=("Creepster", 50), fg="red", bg="black")
+intro.pack(pady=20) 
 
+start = tk.Button(root, text="Start",command=startcommand)
+start.pack()
+start.place(x=900, y=400)
 root.configure(bg="black")
 root.mainloop()
