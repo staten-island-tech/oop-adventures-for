b9c7397a3ace9831ebf6717ca1b43ccc749ec57d
boscot553 <yizhengtian2011@gmail.com>
2025-12-22 10:03:58 -0500

    Update intro.py

--- intro.py
+++ intro.py
@@ -1,8 +1,14 @@
 import tkinter as tk
-
+import time
 
 def startcommand():
-    print("start")
+    start.place_forget()
+    intro.config(text="Loading.")
+    intro.place(x=830, y=400)
+    time.sleep(25)
+    intro.config(text="Loading..")
+    intro.config(text="Loading...")
+    intro.config(text="Loading....")
     #Command here
 
 root = tk.Tk()
@@ -13,8 +19,8 @@ root.geometry("2000x2000")
 intro = tk.Label(root, text="SCARY ELEVATOR", font=("Creepster", 50), fg="red", bg="black")
 intro.pack(pady=20) 
 
-start = tk.Button(root, text="Start",command=startcommand)
+start = tk.Button(root, text="Start",command=startcommand, width= 20, height=3, font=("Creepster", 20), fg="red", bg="#660000")
 start.pack()
-start.place(x=900, y=400)
+start.place(x=830, y=400)
 root.configure(bg="black")
 root.mainloop()
